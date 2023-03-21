9df028b630abb520fd8d0a9b6f58611638370730
DesiAPG <74121435+DesiAPG@users.noreply.github.com>
2023-03-21 15:09:55 -0500

    First Commit

---
+++ src/main/java/com/architecture/microservicesintroduction/MicroservicesIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.architecture.microservicesintroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesIntroductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesIntroductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/architecture/microservicesintroduction/MicroservicesIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.microservicesintroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
